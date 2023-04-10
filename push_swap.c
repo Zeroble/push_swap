c685b387cf377c2c07b839226d5a3cad7167f4bc
Minyong Lee <minylee@cx1r3s1.42seoul.kr>
2023-04-11 01:36:54 +0900

    fucking norminitte

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,12 @@
 /*   By: minylee <minylee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:41:19 by minylee           #+#    #+#             */
-/*   Updated: 2023/04/10 22:35:16 by minylee          ###   ########.fr       */
+/*   Updated: 2023/04/11 01:35:47 by minylee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int				check_errors(t_list *a);
 static int				compare_and_choose(t_list *a, int now);
 static int				is_ascending(t_list *a);
 static long long int	find_min(t_list *lst);
@@ -20,42 +19,97 @@ static void				init_forward(t_list **a, t_list **b);
 static int	find_best_init(t_list *a, int argc, int **ret);
 static int				find_best_value(t_list *start, t_list *end, int *arr);
 static int				*find_path_by_result(int *arr, int argc);
+static void	bubblesort(int *arr, int n);
+static int	find_mid_value(t_list *a, int argc);
+
+static void	step_one(t_list **a, t_list **b, t_list *start, int argc)
+{
+	int	*best_picks;
+	int	best_cnt;
+	int	best_cur;
+	int	cnt;
+	int	ra_before;
+
+	best_cnt = find_best_init(*a, argc, &best_picks);
+	// printf("==========best picks=========\n");
+	// for (int i = 0 ; i < best_cnt ; i++)
+	// 	printf("%d\t-> %d\n", i, best_picks[i]);
+	// printf("==========best picks=========\n");
+	best_cur = 1;
+	cnt = 1;
+	ra(a);
+	ra_before = 1;
+	while ((*a) != start)
+	{
+		printf("next best : %d\tnow count : %d\n", best_picks[best_cur], cnt);
+		if (best_picks[best_cur] == cnt && best_cur != best_cnt)
+		{
+			ra(a);
+			best_cur += 1;
+		}
+		else
+			pb(a, b);
+		cnt += 1;
+	}
+	free(best_picks);
+}
+
+static int	init_and_add_values(t_list **a, t_list **b, int argc, char **argv)
+{
+	int	idx;
+
+	*a = 0;
+	*b = 0;
+	idx = 0;
+	while (++idx < argc)
+		ft_lstadd_back(a, ft_lstnew(ft_atoi(argv[idx])));
+	if (is_ascending(*a) == 1)
+		return (-1);
+	return (1);
+}
+
+static void	step_zero(t_list **a, int argc)
+{
+	int	min_pos;
+	int	cnt;
+
+	cnt = 0;
+	min_pos = find_min(*a);
+	if ((argc - 1) / 2 > min_pos)
+	{
+		while (cnt < min_pos)
+		{
+			ra(a);
+			cnt += 1;
+		}
+	}
+	else
+	{
+		while (cnt < argc - 1 - min_pos)
+		{
+			rra(a);
+			cnt += 1;
+		}
+	}
+}
 
 int	main(int argc, char *argv[])
 {
 	t_list	*a;
 	t_list	*b;
-	int		idx;
-	int		cur2;
-	int		*ret;
 
-	a = 0;
-	b = 0;
-	idx = 0;
-	while (++idx < argc)
-		ft_lstadd_back(&a, ft_lstnew(ft_atoi(argv[idx])));
-	// if (check_errors(a) == -1)
-	// 	exit(-1);
-	idx = 0;
-	// cur2 = find_min(a);
-	// while (idx++ < cur2)
-	// 	ra(&a);
-	// init_forward(&a, &b);
+	init_and_add_values(&a, &b, argc, argv);
+	step_zero(&a, argc);
 	printf("-----------\n");
 	ft_lst_readall(a);
 	printf("-----------\n");
 	ft_lst_readall(b);
-	int best = find_best_init(a, argc, &ret);
-	// printf("%d\n", ret[0]);
-	for (int i = 0 ; i < best ; i++)
-		printf("%d\t->\t%d\n", i, ret[i]);
-}
-
-static int	check_errors(t_list *a)
-{
-	if (is_ascending(a))
-		return (-1);
-	return (1);
+	step_one(&a, &b, a, argc);
+	printf("mid : %d\n", find_mid_value(a, argc));
+	printf("-----------\n");
+	ft_lst_readall(a);
+	printf("-----------\n");
+	ft_lst_readall(b);
 }
 
 static long long int	find_min(t_list *lst)
@@ -98,7 +152,7 @@ static int	is_ascending(t_list *a)
 	return (1);
 }
 
-static int	find_best_init(t_list *a, int argc, int **ret)
+static int	find_best_init(t_list *cur, int argc, int **ret)
 {
 	int		*arr;
 	t_list	*start;
@@ -108,16 +162,19 @@ static int	find_best_init(t_list *a, int argc, int **ret)
 	arr = malloc(sizeof(int) * (argc));
 	arr[0] = 1;
 	cnt = 1;
-	start = a;
-	a = a->next;
+	start = cur;
+	cur = cur->next;
 	arr[argc - 1] = 1;
-	while (start != a)
+	printf("first value : %d\n", start->content);
+	while (start != cur)
 	{
-		arr[cnt] = find_best_value(start, a, arr);
+		printf("now setting arr[%d] (about %d)\n", cnt, cur->content);
+		arr[cnt] = find_best_value(start, cur, arr);
+		printf("set arr[%d] = %d\n", cnt, arr[cnt]);
 		if (arr[cnt] > arr[argc - 1])
 			arr[argc - 1] = arr[cnt];
 		cnt += 1;
-		a = a->next;
+		cur = cur->next;
 	}
 	*ret = find_path_by_result(arr, argc);
 	argc = arr[argc - 1];
@@ -133,7 +190,9 @@ static int	*find_path_by_result(int *arr, int argc)
 
 	idx = argc - 2;
 	val_to_find = arr[argc - 1];
-	printf("val to find : %d\n", val_to_find);
+	for (int i = 0 ; i < argc - 1; i++)
+		printf("%d\t-> %d\n", i, arr[i]);
+	printf("max\t-> %d\n", arr[argc - 1]);
 	ret = malloc(sizeof(int) * (arr[argc - 1]));
 	while (idx != -1)
 	{
@@ -150,17 +209,67 @@ static int	*find_path_by_result(int *arr, int argc)
 static int	find_best_value(t_list *start, t_list *end, int *arr)
 {
 	int	cnt;
-	int	ret;
+	int	max;
 
 	cnt = 1;
 	start = start->next;
-	ret = 1;
+	max = 1;
 	while (start != end)
 	{
-		if (start->content < end->content && arr[cnt] > ret)
-			ret = arr[cnt];
+		if (start->content < end->content && arr[cnt] > max)
+		{
+			printf("change max : %d %d\n", start->content, end->content);
+			max = arr[cnt];
+		}
 		start = start->next;
 		cnt += 1;
 	}
-	return (ret + 1);
+	return (max + 1);
+}
+
+static void	bubblesort(int *arr, int n)
+{
+	int	cur1;
+	int	cur2;
+	int	tmp;
+
+	cur1 = 0;
+	while (cur1 < n - 1)
+	{
+		cur2 = 0;
+		while (cur2 < n - 1 - cur1)
+		{
+			if (arr[cur2] > arr[cur2 + 1])
+			{
+				tmp = arr[cur2];
+				arr[cur2] = arr[cur2 + 1];
+				arr[cur2 + 1] = tmp;
+			}
+			cur2 += 1;
+		}
+		cur1 += 1;
+	}
+}
+
+static int	find_mid_value(t_list *a, int argc)
+{
+	int		*arr;
+	t_list	*start;
+	int		cnt;
+
+	start = a;
+	a = a->next;
+	cnt = 1;
+	arr = malloc(sizeof(int) * (argc - 1));
+	arr[0] = start->content;
+	while (start != a)
+	{
+		arr[cnt] = a->content;
+		a = a->next;
+		cnt += 1;
+	}
+	bubblesort(arr, argc - 1);
+	cnt = arr[(argc - 1) / 2];
+	free(arr);
+	return (cnt);
 }
